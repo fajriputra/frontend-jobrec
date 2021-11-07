0cf171e2c07d7e3e1504ab08dc8fb87aa947582b
Aziz Imastara <sazizimastara@gmail.com>
2021-11-07 22:06:40 +0700

    add component work experience & portofolio

---
+++ src/parts/Profile/Worker/Portofolio/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import porto1 from "../../../../assets/images/porto1.png";
+import porto2 from "../../../../assets/images/porto1.png";
+import porto3 from "../../../../assets/images/porto2.png";
+import porto4 from "../../../../assets/images/porto4.png";
+import porto5 from "../../../../assets/images/porto5.png";
+import porto6 from "../../../../assets/images/porto6.png";
+
+import "./index.scss";
+
+export default function Portofolio(props) {
+  // const urlParams = qs.parse(props.location.search);
+
+  return (
+    <div class="row portofolio">
+      <div class="col portofolio__content">
+        <img src={porto1} alt="" />
+        <p>Remainder app</p>
+      </div>
+      <div class="col portofolio__content">
+        <img src={porto2} alt="" />
+        <p>Social media app</p>
+      </div>
+      <div class="col portofolio__content">
+        <img src={porto3} alt="" />
+        <p>Project management web</p>
+      </div>
+      <div class="col portofolio__content">
+        <img src={porto4} alt="" />
+        <p>Remainder app</p>
+      </div>
+      <div class="col portofolio__content">
+        <img src={porto5} alt="" />
+        <p>Social media app</p>
+      </div>
+      <div class="col portofolio__content">
+        <img src={porto6} alt="" />
+        <p>Project management web</p>
+      </div>
+    </div>
+  );
+}
