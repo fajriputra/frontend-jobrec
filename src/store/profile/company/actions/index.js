122cdfe02ba6b9e3470a8a013f282f7b2a9ab50b
Aziz Imastara <sazizimastara@gmail.com>
2021-11-13 05:52:29 +0700

    edit company form

--- src/store/profile/company/actions/index.js
+++ src/store/profile/company/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "helpers/axios";
 import { PROFILE_PERUSAHAAN } from "../constans";
 import { EDIT_PERUSAHAAN } from "../constans";
+import { EDIT_PERUSAHAAN_IMAGE } from "../constans";
 
 export const profilePerusahaan = (id) => {
   return {
@@ -14,5 +15,12 @@ export const editPerusahaan = (data) => {
   return {
     type: EDIT_PERUSAHAAN,
     payload: axios.patch(`/recruiter/update-profile/`, data),
+  };
+};
+
+export const editPerusahaanImage = (data) => {
+  return {
+    type: EDIT_PERUSAHAAN_IMAGE,
+    payload: axios.patch(`/recruiter/update-recruiter-image/`, data),
   };
 };
--- src/store/profile/company/reducer/index.js
+++ src/store/profile/company/reducer/index.js
@@ -1,4 +1,4 @@
-import { PROFILE_PERUSAHAAN } from "../constans";
+import { EDIT_PERUSAHAAN_IMAGE, PROFILE_PERUSAHAAN } from "../constans";
 import { EDIT_PERUSAHAAN } from "../constans";
 
 const initialState = {
@@ -59,6 +59,35 @@ const profileCompany = (state = initialState, action) => {
     }
 
     case `${EDIT_PERUSAHAAN}_REJECTED`: {
+      return {
+        ...state,
+        isError: true,
+        isLoading: false,
+        data: {},
+        msg: action.payload.response.data.msg,
+      };
+    }
+
+    case `${EDIT_PERUSAHAAN_IMAGE}_PENDING`: {
+      return {
+        ...state,
+        isError: false,
+        isLoading: true,
+        data: {},
+      };
+    }
+
+    case `${EDIT_PERUSAHAAN_IMAGE}_FULFILLED`: {
+      return {
+        ...state,
+        isError: false,
+        isLoading: false,
+        data: action.payload.data.data[0] || {},
+        msg: action.payload.data.msg,
+      };
+    }
+
+    case `${EDIT_PERUSAHAAN_IMAGE}_REJECTED`: {
       return {
         ...state,
         isError: true,
